5176ad632fcd72aba662491951eb2f5de7d28cc5
mohamedpsc <mohamedpsc@gmail.com>
2017-10-23 09:35:42 +0200

    Load Function Now Returns Data Labels

--- database_reader.py
+++ database_reader.py
@@ -1,5 +1,6 @@
 import os
 from scipy.misc import imread
+import numpy
 
 
 '''This Function Load Images dataset from a given directory into ndarray
@@ -15,15 +16,22 @@ def load(dir='orl_faces', train_count=7, test_count=3):
     try:
         training_dataset = []
         test_dataset = []
+        training_label = []
+        test_label = []
         for folder in os.listdir(dir):
-            path = dir+'/'+folder
+            path = dir + '/' + folder
             if os.path.isdir(path):
                 files = os.listdir(path)
                 for i in range(0, train_count):
-                    training_dataset.append(imread(path+ '/' +files[i]).flatten())
+                    training_dataset.append(imread(path + '/' + files[i]).flatten())
+                    training_label.append(folder)
                 for i in range(train_count, train_count + test_count):
                     test_dataset.append(imread(path + '/' + files[i]).flatten())
+                    test_label.append(folder)
     except (NotADirectoryError, IOError, Exception) as e:
         print(e.strerror)
     else:
-        return training_dataset, test_dataset
+        return numpy.asmatrix(training_dataset), numpy.asmatrix(test_dataset), numpy.asmatrix(training_label), numpy.asmatrix(test_label)
+
+if __name__ == '__main__':
+    training, test = load()
