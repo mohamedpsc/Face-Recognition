f31f7d06d683424020276b55f98e4ec064804c4d
mohamedpsc <mohamedpsc@gmail.com>
2017-10-30 12:21:13 +0200

    fixed wrong eigen values and vectors save eigen values and vectors can now be saved in separate files for different number of samples per class functions documentation updated user can choose number of dominant vectors to choose from the eigen vectors added classification method

--- LDA.py
+++ LDA.py
@@ -1,45 +1,72 @@
 import numpy as np
+import database_reader as reader
+from sklearn.neighbors import KNeighborsClassifier
 
-import database_reader
 
-def LDA(train_data, nclasses, spc):
-    """:parameter train_data: training Data set
-    :type train_data: array-like (nd-array, matrix, etc)
-    :parameter nclasses: number of classes included in the given dataset
+def lda(data, nclasses, spc, ndv=None):
+    """
+    :param data: training Data set
+    :type data: array-like (nd-array, matrix, etc)
+    :param nclasses: number of classes included in the given dataset
     :type nclasses: int
-    :parameter spc: Number of samples given per class
-    :type spc: int"""
+    :param spc: Number of samples given per class
+    :type spc: int
+    :param ndv: Number of dominant vectors required
+    :type ndv: int"""
     # Calculate classes means
-    means = np.zeros((nclasses, train_data.shape[1]))
-    for i in range(0, nclasses):
-        means[i] = train_data[i*spc:i*spc+spc, :].mean(0)
+    means = np.zeros((nclasses, data.shape[1]))
+    for i in range(nclasses):
+        means[i] = data[i * spc:i * spc + spc, :].mean(0)
     all_class_mean = means.mean(0)
-    # calculate the mu class Sb
-    sb_matrix = np.zeros((train_data.shape[1], train_data.shape[1]))
-    for i in range(0, nclasses):
+    # calculate the between class scatter matrix Sb
+    sb_matrix = np.zeros((data.shape[1], data.shape[1]))
+    for i in range(nclasses):
         temp = np.subtract(means[i, :], all_class_mean)
         temp = spc * np.matmul(temp.T, temp)
         sb_matrix += temp
     # Calculate Deviation Matrix (Z)
-    dev_matrix = np.zeros(train_data.shape)
+    dev_matrix = np.zeros(data.shape)
     index = 0
-    for i in range(0, nclasses):
-        for j in range(0, spc):
-            dev_matrix[index] = train_data[index] - means[i]
-            index += 1
-    # calculate Scatter matrix (S)
-    scatter_matrix = np.dot(dev_matrix[:spc, :].T, dev_matrix[:spc, :])
     for i in range(nclasses):
+        for j in range(0, spc):
+            dev_matrix[index] = data[index] - means[i]
+            index += 1
+    # calculate within class Scatter matrix (S)
+    scatter_matrix = np.dot(dev_matrix[:spc, :].T, dev_matrix[:spc, :])
+    for i in range(1, nclasses):
         scatter_matrix += np.dot(dev_matrix[i*spc:i*spc+spc, :].T, dev_matrix[i*spc:i*spc+spc, :])
     # Calculate eigen values and vevtors
     eigValues, eigVectors = np.linalg.eigh(np.dot(np.linalg.pinv(scatter_matrix), sb_matrix))
-    np.save('eigvalue', eigVectors)
-    np.save('eigvector', eigValues)
+    np.save('lda_eigvector_'+str(spc), eigVectors)
+    np.save('lda_eigvalue_'+str(spc), eigValues)
+    if ndv is not None:
+        return eigVectors[:, eigVectors.shape[1]-ndv:]
+    return eigVectors
 
-def main():
-    train_data, test_data, train_labels, test_labels = database_reader.load()
-    LDA(train_data, 40, 5)
+
+def lda_classify(nclasses, spc, ndv=None, recompute=False):
+    """
+    :param nclasses: number of classes provided
+    :type nclasses: int
+    :param spc: number of samples per class
+    :type spc: int
+    :param ndv: number of dominant Vectors to be used to project data
+    :type ndv: int
+    :param recompute: if (True), it will re compute LDA even if projection matrix already exist
+    :type recompute: bool
+    :return: accuracy of LDA on the provided train data wrt test data
+    """
+    train_data, test_data, train_labels, test_labels = reader.load()
+    from os import path
+    if path.exists('lda_eigvector_'+str(spc)+'.npy') and not recompute:
+        projection_matrix = np.matrix(np.load('lda_eigvector_'+str(spc)+'.npy'))[:, train_data.shape[1] - ndv:]
+    else:
+        projection_matrix = lda(train_data, nclasses, spc, ndv)
+    projected_data = test_data * projection_matrix
+    neigh = KNeighborsClassifier(n_neighbors=1)
+    neigh.fit(train_data * projection_matrix, train_labels.T)
+    return neigh.score(projected_data, test_labels.T)
 
 
-if __name__=='__main__':
-    main()
+if __name__ == '__main__':
+    print(lda_classify(40, 5, 39))
