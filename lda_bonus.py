81f4eaa378666db35136d122a9425c9dfc64640f
marwan <mrwanbaghdad76@gmail.com>
2017-11-02 23:54:38 +0200

    debug

--- lda_bonus.py
+++ lda_bonus.py
@@ -14,7 +14,7 @@ def compute_in_scatter_matrix(data):
 def computer_between_class_scatter(class1data, class2data):
     logging.info("Start computing between class scatter matrix")
     m1 = class1data.mean(axis=0)
-    m2 = class1data.mean(axis=0)
+    m2 = class2data.mean(axis=0)
     diff = m1 - m2 
     return diff.T * diff
 
@@ -22,7 +22,6 @@ def computer_between_class_scatter(class1data, class2data):
 def lda(inclass_scatter, between_class_scatter):
     logging.info("start lda")
     if  path.exists("lda_bonus_vec.npy") and path.exists("lda_bonus_val.npy"):
-        eig_values, eig_vectors = np.linalg.eigh(np.linalg.inv(between_class_scatter) * inclass_scatter)
         eig_vectors = np.load("lda_bonus_vec.npy")
         eig_values = np.load("lda_bonus_val.npy")
     else:
@@ -30,7 +29,8 @@ def lda(inclass_scatter, between_class_scatter):
         # eig_values = np.flip(eig_values, axis=1) 
         # eig_vectors = np.flip(eig_vectors, axis=1)
         logging.info("Recomputing eigens? press any key to continue")
-        input()
+        # input()
+        eig_values, eig_vectors = np.linalg.eigh(np.linalg.pinv(inclass_scatter) * between_class_scatter)
         np.save("lda_bonus_vec.npy", eig_vectors)
         np.save("lda_bonus_val.npy",eig_values)
 
@@ -40,10 +40,11 @@ def lda_classify_nonhuman(data, dis_vector):
     logging.info("Start classification")
     from sklearn.neighbors import KNeighborsClassifier as KNC 
     knc = KNC(n_neighbors=1)
+    dis_vector = np.asmatrix(dis_vector)
     knc.fit(
-        data[TRAIN_IDX] * dis_vector, 
+        data[TRAIN_IDX] * dis_vector.T, 
         data[TRAIN_LABELS].flatten())
-    return knc.score(data[TEST_IDX] * dis_vector, data[TEST_LABELS].flatten())
+    return knc.score(data[TEST_IDX] * dis_vector.T, data[TEST_LABELS].flatten())
 
 def concatenate_datasets(dataset1, dataset2):
     logging.info("Concatenating datasets")
@@ -59,14 +60,18 @@ if __name__ == "__main__":
     humans[2] = np.ones([1,len(humans[0])])
     humans[3] = np.ones([1,len(humans[1])])
     animals =dbr.load_non_human()
+
     inbetweenClass_scatter = computer_between_class_scatter(humans[0], animals[0])
     animals_scatter = compute_in_scatter_matrix(animals[0])
     humans_scatter = compute_in_scatter_matrix(humans[0])
     all_class_scatter = animals_scatter + humans_scatter
-
-    dis_vector = lda(inbetweenClass_scatter, all_class_scatter)
     print(humans_scatter.shape)
     print(animals_scatter.shape)
+
+    dis_vector = lda(all_class_scatter, inbetweenClass_scatter)
+
+    # dis_vector = lda(None, None)
+
     data = concatenate_datasets(humans, animals)
     print(lda_classify_nonhuman(data, dis_vector))
-    #start classification 
+    
