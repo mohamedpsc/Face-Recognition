11e27a1d99444fa6319530c3c5bd3f6926ff0e1c
marwan <mrwanbaghdad76@gmail.com>
2017-11-02 19:49:50 +0200

    add loading for animals dataset

--- database_reader.py
+++ database_reader.py
@@ -44,6 +44,33 @@ def load(dir='orl_faces', train_count=5, test_count=5):
     else:
         return numpy.asmatrix(training_dataset), numpy.asmatrix(test_dataset), numpy.asmatrix(training_label), numpy.asmatrix(test_label)
 
+def load_non_human():
+    from os.path import listdir, join
+    animals_dir = "cats-dogs-dataset/train"
+    animal_files = listdir(animals_dir)
+    animals_dir = list(map(
+        lambda fname : join(animals_dir, fname),
+        animal_files
+    ))
+    animal_imgs = list(
+        map(
+            preprocessing_data,
+            animal_files
+        )
+    )
+    #TODO construct class for datasets 
+    train.data = numpy.asmatrix(animal_imgs)
+    train.labels = numpy.zeros([1,train.data.shape])
+    
+    
+def preprocessing_data(imgname):
+    '''gresacling and resizing images'''
+    NEW_RES = (92,112)
+    from scipy.misc import imread, imresize
+    img = imread(imgname, mode="LA")
+    img = imresize(img, NEW_RES)
+    return img.flatten()    
+
 if __name__ == '__main__':
     train_data, test_data, train_labels, test_labels = load()
     # numpy.save('Traindata', train_data)
