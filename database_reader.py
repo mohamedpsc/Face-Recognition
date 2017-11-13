576ca356bda7f1e84ff5c011485f0e8c4b9b9414
marwan <mrwanbaghdad76@gmail.com>
2017-11-13 20:16:19 +0200

    constructing humand vs non-humans data for classification

--- database_reader.py
+++ database_reader.py
@@ -1,7 +1,7 @@
 import os
-from scipy.misc import imread
+from scipy.misc import imread,imresize
 import numpy
-
+NEW_RES = (50,50)
 def load(dir='orl_faces', train_count=5):
     '''This Function Load Images dataset from a given directory into numpy.matrix\n
         Args:
@@ -21,6 +21,12 @@ def load(dir='orl_faces', train_count=5):
         test_dataset = []
         training_label = []
         test_label = []
+        def read_img(path, file_):
+            p = os.path.join(path,file_)
+            return imresize(
+                imread(p),
+                NEW_RES
+            ).flatten()
         for folder in os.listdir(dir):
             path = dir + '/' + folder
             if os.path.isdir(path):
@@ -29,67 +35,117 @@ def load(dir='orl_faces', train_count=5):
                 counter = 0
                 for i in range(1, len(files), 2):
                     if counter < train_count:
-                        training_dataset.append(imread(path + '/' + files[i]).flatten())
+                        training_dataset.append(read_img(path,files[i]))
                         training_label.append(folder)
                         counter += 1
                     else:
-                        test_dataset.append(imread(path + '/' + files[i]).flatten())
+                        test_dataset.append(read_img(path,files[i]))
                         test_label.append(folder)
                 for i in range(0, len(files), 2):
                     if counter < train_count:
-                        training_dataset.append(imread(path + '/' + files[i]).flatten())
+                        training_dataset.append(read_img(path,files[i]))
                         training_label.append(folder)
                         counter += 1
                     else:
-                        test_dataset.append(imread(path + '/' + files[i]).flatten())
+                        test_dataset.append(read_img(path,files[i]))
                         test_label.append(folder)
     except (Exception) as e:
         print(e.strerror)
+        print("err")
     else:
         return [numpy.asmatrix(training_dataset), numpy.asmatrix(test_dataset), numpy.asmatrix(training_label), numpy.asmatrix(test_label)]
 
 
-def load_non_human():
+def load_non_human(spc=10):
     from os.path import  join
     from os import listdir
     from itertools import islice
-    animals_dir = "cat-dogs-dataset/train"
+    # animals_dir = "cat-dogs-dataset/train"
+    animals_dir = 'datasets/101_ObjectCategories/airplanes'
     animal_files = listdir(animals_dir)
     animals_files_abs = list(map(
         lambda fname : join(animals_dir, fname),
         animal_files
     ))
+    animal_files_abs = list(
+        filter(
+            lambda x : "cat" in x,
+            animals_files_abs
+        )
+    )
 
     animal_imgs = list(
         map(
             preprocessing_data,
-            animals_files_abs[:200]
+            animals_files_abs[:spc]
         )
     )
 
     animals_tests = list(
         map(
             preprocessing_data,
-            animals_files_abs[200:400]
+            animals_files_abs[spc:spc+spc]
         )
     )
-    train_labels = numpy.zeros([1,len(animal_imgs)])
-    test_labels = numpy.zeros([1,len(animals_tests)])
 
-    return  [numpy.asmatrix(animal_imgs), numpy.asmatrix(animals_tests), train_labels,test_labels]
+    train_labels = numpy.zeros([1,spc])
+    test_labels = numpy.zeros([1,spc])
+    ans = []
+    ans.append(
+        (
+            numpy.asmatrix(animal_imgs),
+            numpy.asmatrix(animals_tests),
+            train_labels,
+            test_labels
+        )
+    )
+
+    h = load()
+    ans.append(
+        (
+            h[0][0:spc,:],
+            h[1][0:spc, :],
+            numpy.ones([1,(spc)]),
+            numpy.ones([1,(spc)])
+        )
+    )
+    z = zip(ans[0], ans[1])
+    ans = []
+    for i,j in z:
+        # print(i.shape,j.shape)
+        # assert i.shape == j.shape
+        ans.append(
+            numpy.vstack((
+                i,
+                j
+            ))
+        )
     
+    ans[2] = ans[2].flatten()
+    ans[3] = ans[3].flatten()
+    temp = ans[1] 
+    ans[1] = ans[2] 
+    ans[2] = temp
+    del(temp)
+    for i in ans:
+        print(i.shape)
+    return  iter(ans)
+
 import logging
 logging.basicConfig(level=logging.INFO)
 def preprocessing_data(imgname):
     '''gresacling and resizing images'''
     logging.debug(imgname)
-    NEW_RES = (92,112)
     from scipy.misc import imread, imresize
     img = imread(imgname, mode="L")
     img = imresize(img, NEW_RES)
-    return img.flatten()    
+    return img.flatten()
 
 
 if __name__ == '__main__':
-    train_data, test_data, train_labels, test_labels = load(train_count=7)
+    # train_data, test_data, train_labels, test_labels = load(train_count=7)
+    from pprint import pprint
+    print(
+        list(load_non_human())[3]
+        )
 
